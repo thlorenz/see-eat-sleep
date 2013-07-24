aea9f00c8df98bc48f9cecbc590e9f1a1308f64e
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-24 17:43:51 -0400

    adding export to see index

--- see/client/see.js
+++ see/client/see.js
@@ -3,6 +3,11 @@
 var noncjs = require('noncjs');
 var core = require('ses-core');
 
-console.log('noncjs is same instance as core.noncjs', noncjs === core.noncjsThing);
+console.log('sourcing see');
 
+console.log('noncjs is same instance as core.noncjs', noncjs === core.noncjsThing);
 console.log('Whats that noncjs thing?\n >> ', core.noncjsThing());
+
+exports.init = function () {
+  console.log('initializing see');
+};
