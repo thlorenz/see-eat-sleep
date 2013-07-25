0d8b78977c135926a7b89206b257c09dd3d8c5fd
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-25 16:57:06 -0400

    don't need core in main app

--- app/preinstall.sh
+++ app/preinstall.sh
@@ -4,10 +4,6 @@
 ( cd ../bootstrap && npm install && npm dedupe )
 [ -d ./node_modules/ses-bootstrap ] || ln -s ../../bootstrap ./node_modules/ses-bootstrap
 
-# core
-( cd ../core && npm install && npm dedupe )
-[ -d ./node_modules/ses-core ] || ln -s ../../core ./node_modules/ses-core
-
 # see
 ( cd ../see && npm install && npm dedupe )
 [ -d ./node_modules/ses-see ] || ln -s ../../see ./node_modules/ses-see
