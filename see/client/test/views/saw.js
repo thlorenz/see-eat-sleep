0550ad5495b4a06057f0c431bc3eb7575c076f24
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-30 15:53:09 -0400

    mocha friendly jshintrcs

--- see/client/test/views/saw.js
+++ see/client/test/views/saw.js
@@ -1,11 +1,15 @@
 'use strict';
 
+var $ = require('jquery');
 var assert = require('assert');
 var localBus = require('../../lib/local-bus');
-var sawView = require('../../views/saw');
+var SawView = require('../../views/saw');
 
 describe('a test', function () {
+  var sawButton = $('#ses-see-main .ses-see-saw');
+  var sawView = new SawView(sawButton);
+
   it('!!1 is true', function () {
     assert.equal(!!1, true);
-  })
-})
+  });
+});
