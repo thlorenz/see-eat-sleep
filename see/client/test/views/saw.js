deb01acc367824dfefb572f8295b78354d6992b1
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-30 11:31:48 -0400

    adding mocha test

---
+++ see/client/test/views/saw.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+var localBus = require('../../lib/local-bus');
+var sawView = require('../../views/saw');
+
+describe('a test', function () {
+  it('!!1 is true', function () {
+    assert.equal(!!1, true);
+  })
+})
