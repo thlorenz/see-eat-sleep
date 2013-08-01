36bfa4736ef4effca988f4f167e07c05eb1d4d8c
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-01 17:29:54 -0400

    serving acutal sight urls

---
+++ see/servers/api/data/sights.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var go = module.exports = [
+  'http://upload.wikimedia.org/wikipedia/commons/a/a0/Potsdam_St._Nikolaikirche_2005.jpg',
+  'http://www.europeinsideout.com/wp-content/uploads/2012/03/berlin-brandenburger_tor-300x199.jpg',
+  'http://upload.wikimedia.org/wikipedia/commons/e/e0/Nauener_Tor_Potsdam.JPG',
+  'http://fc02.deviantart.net/fs70/i/2011/173/9/a/potsdam___sanssouci_park_by_pingallery-d3jniwc.jpg',
+  'http://www.germanplaces.com/templates/berlin-germany/images/berlin-germany-museum-small.jpg',
+  'http://www.reise-abc.ch/berlin/images/spree-berlin-3.jpg',
+  'http://www.detlef-henke.de/wp-content/uploads/friedrich_schiller_berlin_konzerthaus_denkmal.jpg',
+];
--- see/servers/api/routes/sights.js
+++ see/servers/api/routes/sights.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var sights = require('../data/sights');
+
 var go = module.exports = function (app) {
   app.get('/data/sights', function (req, res) {
     res.header('Content-Type', 'application/json');
-    res.send([ 'url1', 'url2', 'url2' ]);
+    res.send(sights);
   });
 };
