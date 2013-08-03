f484746b3d0e67633a3106cfd458b4b50047840c
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-03 16:41:48 -0400

    adding static route to zuul config and adding more testing libs to fixture

--- see/zuul-config.js
+++ see/zuul-config.js
@@ -6,3 +6,10 @@ var testroot = path.join(__dirname, 'client', 'test');
 exports.fixture = function () {
   return fs.readFileSync(path.join(testroot, 'fixture.html'), 'utf8');
 };
+
+exports.initApp = function (app, express) {
+  var sinonlib = path.dirname(require.resolve('sinon'));
+  console.error('adding "/sinon" static folder pointing at %s', sinonlib);
+  
+  app.use('/sinon', express.static(sinonlib));
+};
