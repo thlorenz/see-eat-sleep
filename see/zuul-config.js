3f4ebfab68d9235b2fd7be92e43d7d9c39ed6249
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-03 18:34:03 -0400

    adding styles back to tests

--- see/zuul-config.js
+++ see/zuul-config.js
@@ -1,8 +1,10 @@
 'use strict';
 var fs = require('fs');
 var path = require('path');
+var dirs = require('./config/directories');
 var testroot = path.join(__dirname, 'client', 'test');
 
+var sinonpkg = path.join(path.dirname(require.resolve('sinon')), '..', 'pkg', 'sinon.js');
 
 exports.fixture = function () {
   return fs.readFileSync(path.join(testroot, 'fixture.html'), 'utf8');
@@ -10,8 +12,7 @@ exports.fixture = function () {
 
 exports.initApp = function (app, express) {
   app.use(express.logger('dev'));
-
-  var sinonpkg = path.join(path.dirname(require.resolve('sinon')), '..', 'pkg', 'sinon.js');
+  app.use('/ses-see-css', express.static(dirs.css));
 
   app.get('/sinon.js', function (req, res) {
     res.sendfile(sinonpkg);
