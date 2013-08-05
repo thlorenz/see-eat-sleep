63f8507c66681a3cb1ee4c20094e0847f209336f
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-05 17:57:32 -0400

    starting to phase out bootstrap

--- bootstrap/lib/pages/dev-server.js
+++ bootstrap/lib/pages/dev-server.js
@@ -19,19 +19,6 @@ var go = module.exports = function (pagesOpts, buildOpts) {
 
   app.set('view engine', 'hbs');
 
-  // serve the index file given to us by the app
-  // it needs to include our partials in order to be setup correctly
-  app.get('/', function (req, res, next) {
-
-    // recompile template on every refresh in development to reflect any changes to ie
-    fs.readFile(pagesOpts.index, 'utf8', function (err, tmpl) {
-      if (err) return next(err);
-      res.set({ 'Content-Type': 'text/html' });
-      var html = Handlebars.compile(tmpl)(pagesOpts.context);
-      res.send(200, html);
-    });
-  });
-
   // serve the bundle that we require in our script-body-js template
   app.get('/bundle.js', function (req, res, next) {
     build(buildOpts, function (err, bundle) {
