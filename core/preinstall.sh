27669879bd447f5c710f8de70493a5830a799d5f
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-06 10:53:26 -0400

    removing bootstrap and old dev scripts from core

--- core/preinstall.sh
+++
@@ -1,5 +0,0 @@
-[ -d node_modules ] || mkdir node_modules
-
-( cd ../bootstrap && npm install && npm dedupe )
-
-[ -d ./node_modules/ses-bootstrap ] || ln -s ../../bootstrap ./node_modules/ses-bootstrap
