072a0a532b9c8e754552ba17177ed7e7cf7db25d
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-06 11:23:00 -0400

    removin bootstrap from see and shims since they are no longer needed

--- see/preinstall.sh
+++ see/preinstall.sh
@@ -1,9 +1,5 @@
 [ -d node_modules ] || mkdir node_modules
 
-# bootstrap
-( cd ../bootstrap && npm install && npm dedupe )
-[ -d ./node_modules/ses-bootstrap ] || ln -s ../../bootstrap ./node_modules/ses-bootstrap
-
 # core
 ( cd ../core && npm install && npm dedupe )
 [ -d ./node_modules/ses-core ] || ln -s ../../core ./node_modules/ses-core
