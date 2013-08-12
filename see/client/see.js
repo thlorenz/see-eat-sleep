e751c0974c42c973cbad1f85d43cacc6e4f8e99b
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-12 11:56:57 -0400

    simplifying things

--- see/client/see.js
+++ see/client/see.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var $ = require('jquery');
-
 var MainView = require('./views/main');
 
 exports.init = function () {
-  return new MainView({ el: $('#ses-see-main') });
+  return new MainView({ el: '#ses-see-main' });
 };
--- see/client/views/main.js
+++ see/client/views/main.js
@@ -3,11 +3,10 @@
 var SawView = require('./saw');
 var SightsView = require('./sights');
 var Backbone = require('backbone');
-var $ = require('jquery');
 
 module.exports = Backbone.View.extend({
   initialize: function () {
-    this.sawView = new SawView({ el: this.$el.find('.ses-see-saw') });
-    this.sightsView = new SightsView({ el: this.$el.find('.ses-see-sights') });
+    this.sawView = new SawView({ el: this.$('.ses-see-saw') });
+    this.sightsView = new SightsView({ el: this.$('.ses-see-sights') });
   }
 });
