45656ff81bc421c8b403d01259c751a53bedb290
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-21 17:58:12 -0400

    reinstall and local reinstall all scripts

---
+++ local-reinstall-all.sh
@@ -0,0 +1,5 @@
+npm rm -S thlorenz/ses-core  && npm i -S ../core
+npm rm -S thlorenz/ses-aside && npm i -S ../aside
+npm rm -S thlorenz/ses-see   && npm i -S ../see
+npm rm -S thlorenz/ses-eat   && npm i -S ../eat
+npm rm -S thlorenz/ses-sleep && npm i -S ../sleep
---
+++ reinstall-all.sh
@@ -0,0 +1,5 @@
+npm rm -S thlorenz/ses-core  && npm i -S thlorenz/ses-core
+npm rm -S thlorenz/ses-aside && npm i -S thlorenz/ses-aside
+npm rm -S thlorenz/ses-see   && npm i -S thlorenz/ses-see
+npm rm -S thlorenz/ses-eat   && npm i -S thlorenz/ses-eat
+npm rm -S thlorenz/ses-sleep && npm i -S thlorenz/ses-sleep
